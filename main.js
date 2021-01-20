0543412aa4748b328ce9a3858dd1ecd67735aa21
Sharief <sysharief@gmail.com>
2021-01-21 00:39:47 +0530

    commit mine

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return{
+            product:"Shoes",
+            description:"Good Quality SHoes are available"
+        }
+    }
+});
